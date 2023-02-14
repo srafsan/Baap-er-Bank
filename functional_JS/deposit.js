84b9191982222d8a42564200344f202389d663a5
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-02-14 20:53:24 +0600

    Done error handling

--- functional_JS/deposit.js
+++ functional_JS/deposit.js
@@ -1,5 +1,11 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
     const newDepositAmount = getInputValueById("deposit-field");
+
+    if (isNaN(newDepositAmount)) {
+        alert("Please provide a valid number");
+        return;
+    }
+
     const prevDepositTotal = getElementValueById("deposit-total");
     const depositTotal = prevDepositTotal + newDepositAmount;
     setTextElementValueById("deposit-total", depositTotal);
--- functional_JS/withdraw.js
+++ functional_JS/withdraw.js
@@ -1,10 +1,22 @@
 document.getElementById("btn-withdraw").addEventListener("click", function () {
     const newWithdrawAmount = getInputValueById("withdraw-field");
+
+    if (isNaN(newWithdrawAmount)) {
+        alert("Please provide a valid number");
+        return;
+    }
+
     const prevWithdrawTotal = getElementValueById("withdraw-total");
+    const prevBalanceTotal = getElementValueById("balance-total");
+
+    if (newWithdrawAmount > prevBalanceTotal) {
+        alert("Baap er bank e eto taka nei");
+        return;
+    }
+
     const withdrawTotal = prevWithdrawTotal + newWithdrawAmount;
     setTextElementValueById("withdraw-total", withdrawTotal);
 
-    const prevBalanceTotal = getElementValueById("balance-total");
     const newBalanceTotal = prevBalanceTotal - newWithdrawAmount;
     setTextElementValueById("balance-total", newBalanceTotal);
 });
